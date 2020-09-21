17bec500575c893cff198b5943b2bdc976631d36
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-21 17:28:12 -0500

    created pokemon container, and display every pokemon there

--- src/containers/Pokecontainer.js
+++ src/containers/Pokecontainer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Loading from '../components/Loading';
+import Pokemon from '../components/Pokemon';
 
 class Pokecontainer extends React.Component {
   constructor() {
@@ -8,8 +10,23 @@ class Pokecontainer extends React.Component {
       nextPokemons: 'https://pokeapi.co/api/v2/pokemon?limit=25',
       loading: true,
       prevY: 0,
-    }
+    };
     this.loadingRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.getPokemons();
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
+    };
+
+    const observer = new IntersectionObserver(
+      this.handleObserver.bind(this),
+      options,
+    );
+    observer.observe(this.loadingRef.current);
   }
 
   async getPokemons() {
@@ -17,47 +34,36 @@ class Pokecontainer extends React.Component {
     const { nextPokemons } = this.state;
     const response = await fetch(nextPokemons);
     const data = await response.json();
-    this.setState((state) => ({
+    this.setState(state => ({
       pokemons: [...state.pokemons, ...data.results],
       nextPokemons: data.next,
       loading: false,
     }));
   }
 
-  handleObserver(entries, observer) {
-    const { prevY } = this.state;
-    const y = entries[0].boundingClientRect.y;
-    if (prevY > y){
+  handleObserver(entries) {
+    const { prevY, nextPokemons } = this.state;
+    const { y } = entries[0].boundingClientRect;
+    if (prevY > y && nextPokemons) {
       this.getPokemons();
     }
-    this.setState({ prevY: y })
-  }
-
-  componentDidMount(){
-    this.getPokemons();
-    let options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0,
-    }
-
-    let observer = new IntersectionObserver(this.handleObserver.bind(this), options);
-    observer.observe(this.loadingRef.current);
+    this.setState({ prevY: y });
   }
 
   render() {
-    const { pokemons } = this.state;
-    const loadingCss = { display: this.state.loading ? "block" : "none" }
-    const loadingDiv = { height: "100px", margin: "10px" }
-    return(
-      <div>
-        { 
-        pokemons.map(pokemon => (
-          <div key={pokemon.name}>{pokemon.name}</div>
-        ))
-        }
+    const { pokemons, loading } = this.state;
+    const loadingCss = { display: loading ? 'block' : 'none' };
+    const loadingDiv = { height: '100px', width: '100%', margin: '10px' };
+    return (
+      <div className="flex flex-wrap container mx-auto">
+        {pokemons.map((pokemon, index) => (
+          // eslint-disable-next-line react/no-array-index-key
+          <Pokemon index={index} pokemon={pokemon} key={index} />
+        ))}
         <div style={loadingDiv} ref={this.loadingRef}>
-          <span style={loadingCss}>Loading...</span>
+          <div style={loadingCss}>
+            <Loading />
+          </div>
         </div>
       </div>
     );
