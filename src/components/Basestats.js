710732fdfed1c753a59bc5de34a7f385b1de3649
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-22 15:42:46 -0500

    created the Basestats component to show the stats of the pokemon

--- src/components/Basestats.js
+++ src/components/Basestats.js
@@ -2,7 +2,23 @@ import React from 'react';
 import PropType from 'prop-types';
 
 function Basestats({ stats }) {
-  return <div>Base Stats</div>;
+  return (
+    <div className="max-w-md mx-auto text-left">
+      <span className="font-extrabold">Base Stats</span>
+      {stats.map(stat => (
+        <div className="w-full" key={stat.stat.name}>
+          <div className="w-full shadow-inner">
+            <div
+              className="bg-blue-500 text-sm leading-none py-1 text-white text-left"
+              style={{ width: `${(stat.base_stat * 100) / 255}%` }}
+            />
+            <span className="capitalize ml-3">{`${stat.stat.name}: `}</span>
+            <span>{`${stat.base_stat} / 255`}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 Basestats.propTypes = {
